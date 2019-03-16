1f4e0d64cf851e307a33e594aebb3a465e1996c5
JQ <test@jq.com>
2019-03-16 09:41:39 -0700

    Added lastIndexOf challenge

---
+++ lastIndexOf.js
@@ -0,0 +1,17 @@
+// Write a function called lastIndexOf, which accepts an array and a number.
+
+// The function should return the last index at which the value exists, or -1 if the value is not found.
+
+// Do not use the built in Array.lastIndexOf() function!
+
+function lastIndexOf(arr, num){
+
+    for (let i = arr.length; i > 0; i--){
+      if (num === arr[i]){
+        return i;
+      }
+    }
+    return -1;
+  }
+
+// lastIndexOf([1, 2, 3, 2], 2)
